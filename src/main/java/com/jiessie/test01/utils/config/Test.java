3c57b50e6f2542b18f58c9521dd64ae1123c0b1d
huangmj <huangmj@bokecc.com>
2020-09-17 10:35:44 +0800

    Revert "第一次提交0001"
    This reverts commit e98a966

--- src/main/java/com/jiessie/test01/utils/config/Test.java
+++ src/main/java/com/jiessie/test01/utils/config/Test.java
@@ -4,7 +4,5 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("第一次提交0001");
-        System.out.println("第一次提交0002");
-        System.out.println("第一次提交0003");
     }
 }
