e98a9665db5a32831eea2218692194e94a5c1c5f
huangmj <huangmj@bokecc.com>
2020-09-17 10:20:59 +0800

    第一次提交0001

--- src/main/java/com/jiessie/test01/utils/config/Test.java
+++ src/main/java/com/jiessie/test01/utils/config/Test.java
@@ -3,6 +3,6 @@ package com.jiessie.test01.utils.config;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("第一次提交001");
+        System.out.println("第一次提交0001");
     }
 }
